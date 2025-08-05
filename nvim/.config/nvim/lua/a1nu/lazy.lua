ceb474e4bbf9bf4e32f0569b6a1032d774c89a34
A1nu <ainu.develop@gmail.com>
2025-08-05 23:46:00 +0300

    Adding nvim configuration

--- nvim/.config/nvim/lua/a1nu/lazy.lua
+++ nvim/.config/nvim/lua/a1nu/lazy.lua
@@ -11,4 +11,14 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup({ { import = "a1nu.plugins" }, { import = "a1nu.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
+
 require("lazy").setup("a1nu.plugins")
